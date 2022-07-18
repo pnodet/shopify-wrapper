30d4b6bdba9c13a0bc06113150b7e05046948b49
pnxdxt <paul.nodet@gmail.com>
2022-07-18 13:19:58 +0200

    test: add dotenv to get secret variables

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,17 @@
 import {describe, it, expect} from 'vitest';
 import shopifyWrapper from '../src';
+import 'dotenv/config.js';
+
+const config = {
+	domain: process.env.DOMAIN!,
+	token: process.env.TOKEN!,
+};
 
 describe('product', () => {
 	it('find', async () => {
-		const result = await shopifyWrapper.product.find({
-			handle: 'test',
-			config: {
-				domain: 'test',
-				token: 'test',
-			},
+		const result = await shopifyWrapper.product.findMany({
+			amount: 10,
+			config,
 		});
 		expect(result).toBeDefined();
 	});
